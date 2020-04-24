ddddfe3d0e78023588cbbaea49cd22cfd2e27d11
thankuuuu <53027899+thankuuuu@users.noreply.github.com>
2020-04-23 22:00:10 -0400

    Update chartgender.js

--- docs/js/chartgender.js
+++ docs/js/chartgender.js
@@ -41,6 +41,7 @@ var Charts = /** @class */ (function () {
                 ]
             },
             options: {
+                responsive: false,
                 scale: {
                     pointLabels: {
                         fontColor: this.tickColor
